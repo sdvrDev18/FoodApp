47cbcf85746b650055ca5b1d814740f2df741ef5
navaneeth323 <navaneethallpeml@gmail.com>
2021-06-12 12:10:27 +0530

    sidebar commit 1

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <>
+      <nav className="sidebar">
+        <div className="nav-links">
+          <a className="sidebar-link">Link1</a>
+          <a className="sidebar-link">Link2</a>
+          <a className="sidebar-link">Link3</a>
+          <a className="sidebar-link">Link4</a>
+
+        </div>
+
+
+      </nav>
+    </>
+  )
+}
+
+export default Sidebar
